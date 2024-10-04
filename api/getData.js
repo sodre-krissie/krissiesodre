2239de83f4dce547e7e05b93dc834e70a8fe28b5
KSodre <sodre.krissie@gmail.com>
2024-10-04 17:04:17 -0300

    atualização geral

--- api/getData.js
+++ api/getData.js
@@ -1,29 +1,49 @@
-// /api/getData.js
-let dados = []; // Simulação de um banco de dados em memória
+import { promises as fs } from 'fs';
+import path from 'path';
 
-export default function handler(req, res) {
+const filePath = path.join(process.cwd(), 'medcard', 'assets', 'json', 'dados.json');
+
+// Função para ler os dados do arquivo JSON
+async function lerDados() {
+    const dadosJson = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(dadosJson);
+}
+
+// Função para escrever os dados no arquivo JSON
+async function escreverDados(dados) {
+    await fs.writeFile(filePath, JSON.stringify(dados, null, 2), 'utf-8');
+}
+
+export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { disciplina, pergunta, resposta } = req.body;
-        dados.push({
-            disciplina,
-            pergunta,
-            resposta
-        });
+        const dados = await lerDados();
+
+        dados.push({ disciplina, pergunta, resposta });
+        await escreverDados(dados);
+
         res.status(201).json({ message: 'Dados cadastrados com sucesso!' });
     } else if (req.method === 'GET') {
+        const dados = await lerDados();
         res.status(200).json(dados); // Retorna todos os dados cadastrados
     } else if (req.method === 'DELETE') {
         const { index } = req.body;
+        const dados = await lerDados();
+
         if (dados[index]) {
             dados.splice(index, 1);
+            await escreverDados(dados);
             res.status(200).json({ message: 'Dados excluídos com sucesso!' });
         } else {
             res.status(404).json({ message: 'Dado não encontrado.' });
         }
     } else if (req.method === 'PUT') {
         const { index, updatedData } = req.body; // Obtém o índice e os dados atualizados
+        const dados = await lerDados();
+
         if (dados[index]) {
             dados[index] = updatedData; // Atualiza o item no array
+            await escreverDados(dados);
             res.status(200).json({ message: 'Dados atualizados com sucesso!' });
         } else {
             res.status(404).json({ message: 'Dado não encontrado.' });
