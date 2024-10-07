9300b81d328426e5a0dd60a7947ac40dcde79ecf
KSodre <sodre.krissie@gmail.com>
2024-10-07 20:16:25 -0300

    att

--- api/saveFlashcard.js
+++ api/saveFlashcard.js
@@ -1,7 +1,16 @@
 // /api/saveFlashcard.js
 
 import pkg from 'pg';
-const { Client } = pkg;
+const { Pool } = pkg;
+
+// Cria um pool de conexões
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT, 10),
+});
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
@@ -9,16 +18,9 @@ export default async function handler(req, res) {
 
         console.log('Dados recebidos:', { disciplina, pergunta, resposta }); // Log dos dados recebidos
 
-        const client = new Client({
-            user: process.env.DB_USER,
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            password: process.env.DB_PASSWORD,
-            port: parseInt(process.env.DB_PORT, 10),
-        });
-
         try {
-            await client.connect();
+            // Conexão do pool
+            const client = await pool.connect();
             console.log('Conexão com o banco de dados estabelecida.'); // Log de conexão
 
             const query = 'INSERT INTO cad_flashcard (disciplina, pergunta, resposta) VALUES ($1, $2, $3)';
@@ -26,14 +28,14 @@ export default async function handler(req, res) {
 
             await client.query(query, values);
             console.log('Flashcard cadastrado com sucesso!'); // Log de sucesso
-            await client.end();
 
             res.status(200).json({ message: 'Flashcard cadastrado com sucesso!' });
         } catch (error) {
             console.error('Erro ao inserir flashcard:', error); // Log do erro para diagnóstico
             res.status(500).json({ message: 'Erro ao cadastrar flashcard', error: error.message });
         } finally {
-            await client.end(); // Assegura que o cliente será fechado
+            // Libera a conexão de volta para o pool
+            client.release();
         }
     } else {
         res.setHeader('Allow', ['POST']);
