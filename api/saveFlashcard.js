e9defa4d3d1566f0ca73f7e9509d59fd8cdc659b
KSodre <sodre.krissie@gmail.com>
2024-10-07 20:15:55 -0300

    att

--- api/saveFlashcard.js
+++ api/saveFlashcard.js
@@ -1,33 +1,39 @@
 // /api/saveFlashcard.js
 
 import pkg from 'pg';
-const { Client } = pkg; // Desestruturação para obter o Client
-
-const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT, 10),
-});
+const { Client } = pkg;
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { disciplina, pergunta, resposta } = req.body;
 
+        console.log('Dados recebidos:', { disciplina, pergunta, resposta }); // Log dos dados recebidos
+
+        const client = new Client({
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: parseInt(process.env.DB_PORT, 10),
+        });
+
         try {
             await client.connect();
+            console.log('Conexão com o banco de dados estabelecida.'); // Log de conexão
 
             const query = 'INSERT INTO cad_flashcard (disciplina, pergunta, resposta) VALUES ($1, $2, $3)';
             const values = [disciplina, pergunta, resposta];
 
             await client.query(query, values);
+            console.log('Flashcard cadastrado com sucesso!'); // Log de sucesso
             await client.end();
 
             res.status(200).json({ message: 'Flashcard cadastrado com sucesso!' });
         } catch (error) {
-            console.error('Error inserting flashcard:', error); // Log do erro para diagnóstico
+            console.error('Erro ao inserir flashcard:', error); // Log do erro para diagnóstico
             res.status(500).json({ message: 'Erro ao cadastrar flashcard', error: error.message });
+        } finally {
+            await client.end(); // Assegura que o cliente será fechado
         }
     } else {
         res.setHeader('Allow', ['POST']);
